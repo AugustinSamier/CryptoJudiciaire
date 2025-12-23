d25c7a48f649c8fa6420e421c3ceb839ebc6e7d8
AugustinSamier <augustinsamier@gmail.com>
2025-12-23 14:24:19 +0100

    Commit avancements

--- SimpleGraph.py
+++ SimpleGraph.py
@@ -3,34 +3,36 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import argparse
 
-def make_graph(G,address,addresses=None):
-    url = f"https://api.kas.fyi/v1/addresses/{address}/transactions"
+def make_graph(G,address,limit,addresses=None):
+    url = f"https://api.kas.fyi/v1/addresses/{address}/transactions?limit={limit}"
     headers = {"x-api-key": API_KEY}
 
     response = requests.get(url, headers=headers)
     data = response.json()
     transac={}
-    for transactions in data["transactions"]:
-        for input in transactions["inputs"]:
-            incomeAddress=input["previousOutput"]["scriptPublicKeyAddress"]
-            if incomeAddress not in transac:
-                transac[incomeAddress]={}
-            for output in transactions["outputs"]:
-                outcomeAddress=output["scriptPublicKeyAddress"]
-                if outcomeAddress not in transac[incomeAddress]:
-                    transac[incomeAddress][outcomeAddress]=1
-                else:
-                    transac[incomeAddress][outcomeAddress]+=1
-    if addresses is None:
-        addresses=[]
-    for income in transac:
-        if income not in addresses:
-            addresses.append(income)
-        for outcome in transac[income]:
-            G.add_edge(income[:15],outcome[:15],weight=transac[income][outcome])
-            if outcome not in addresses:
-                addresses.append(outcome)
-
+    try:
+        for transactions in data["transactions"]:
+            for input in transactions["inputs"]:
+                incomeAddress=input["previousOutput"]["scriptPublicKeyAddress"]
+                if incomeAddress not in transac:
+                    transac[incomeAddress]={}
+                for output in transactions["outputs"]:
+                    outcomeAddress=output["scriptPublicKeyAddress"]
+                    if outcomeAddress not in transac[incomeAddress]:
+                        transac[incomeAddress][outcomeAddress]=1
+                    else:
+                        transac[incomeAddress][outcomeAddress]+=1
+        if addresses is None:
+            addresses=[]
+        for income in transac:
+            if income not in addresses:
+                addresses.append(income)
+            for outcome in transac[income]:
+                G.add_edge(income[:15],outcome[:15],weight=transac[income][outcome])
+                if outcome not in addresses:
+                    addresses.append(outcome)
+    except:
+        print("ERREUR : ",address)
     return G,addresses
 
 
@@ -50,14 +52,20 @@ def main(args):
     G=nx.DiGraph()
     allOutcomes=[]
     allOutcomes.append([outcome])
+    allAddresses=[]
+    allAddresses.extend([outcome])
+    print(len(allAddresses))
     colors=[]
     for cercle in range(nb_cercles):
         print("Size : ",len(allOutcomes[cercle]))
         for i in range(len(allOutcomes[cercle])):
             colors.append(COLOR[cercle])
             print(allOutcomes[cercle][i])
-            G,outcomes=make_graph(G,allOutcomes[cercle][i])
-            allOutcomes.append(outcomes)
+            G,outcomes=make_graph(G,allOutcomes[cercle][i],limit=args.limit)
+            if outcomes is not None:
+                allOutcomes.append(outcomes)
+                allAddresses.extend(outcomes)
+            print(len(allAddresses))
     for i in range(len(G)-len(colors)):
         colors.append(COLOR[nb_cercles-1])
     print(len(G))
@@ -88,6 +96,7 @@ if __name__=="__main__":
     parser=argparse.ArgumentParser(description="Arguments")
     parser.add_argument("--address",type=str,help="address Kaspa")
     parser.add_argument("--APIkey",type=str,help="API key")
+    parser.add_argument("--limit",type=int,default=3,help="Limite de transactions")
     parser.add_argument("--nbCercles",type=int,default=3,help="Nombre de cercles")
     args=parser.parse_args()
     API_KEY=args.APIkey
